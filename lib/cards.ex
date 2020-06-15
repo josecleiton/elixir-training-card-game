b8cc7d7e122f63b855d27fc822586f00ae22b6c5
José Cleiton <jcleitonbc@gmail.com>
2020-06-15 10:00:05 -0300

    test and docs

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,25 +1,15 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for `Cards`.
+    Provides methods for creating and handling a deck of cards
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
-  @type card :: charlist()
+  @type card :: charlist
   @type deck :: [card, ...]
   @type hand :: deck
 
+  @doc """
+    Return a list of strings representing a deck
+  """
   @spec create_deck :: deck
   def create_deck do
     values = [
@@ -45,31 +35,64 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Shuffle the deck
+  """
   @spec shuffle(deck) :: deck
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains given card
+
+  ## Examples
+
+        iex> deck = Cards.create_deck
+        iex> Cards.contains?(deck, "Ace of Diamonds")
+        true
+
+        iex> deck = Cards.create_deck
+        iex> Cards.contains?(deck, "random_card")
+        false
+
+  """
   @spec contains?(deck, card) :: boolean
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divide a deck into a hand and the remainder of the deck.
+    The `hand_size` indicates how many cards will be placed in hand
+
+  ## Examples
+
+        iex> deck = Cards.create_deck
+        iex> {hand, deck} = Cards.deal(deck, 1)
+        iex> hand
+        ["Ace of Spades"]
+  """
   @spec deal(deck, number) :: {hand, deck}
   def deal(deck, hand_size) do
     if hand_size <= 0 do
-      raise "Hand size should be greater then 0"
+      raise "Hand size should be greater than 0"
     end
 
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Save deck in fs
+  """
   @spec save(deck, charlist) :: :ok
   def save(deck, filename) do
-    content = :erlang.term_to_binary(deck)
-    File.write(filename, content)
+    File.write(filename, :erlang.term_to_binary(deck))
   end
 
+  @doc """
+    Load deck from fs
+  """
   @spec load(charlist) :: deck
   def load(filename) do
     case File.read(filename) do
@@ -78,7 +101,10 @@ defmodule Cards do
     end
   end
 
-  @spec create_hand(number) :: hand
+  @doc """
+    Pick a hand with given `hand_size` from shuffled deck
+  """
+  @spec create_hand(number) :: {hand, deck}
   def create_hand(hand_size) do
     Cards.create_deck() |> Cards.shuffle() |> Cards.deal(hand_size)
   end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,19 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck makes 52 cards" do
+    assert length(Cards.create_deck()) === 52
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck()
+    refute deck === Cards.shuffle(deck)
+  end
+
+  test "create hand with given hand_size" do
+    hand_size = 3
+    {hand, deck} = Cards.create_hand(hand_size)
+    assert length(hand) === hand_size
+    assert length(deck) === 52 - hand_size
   end
 end
